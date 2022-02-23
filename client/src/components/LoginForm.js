d6180b3f676fc8514b1b6087431ef7f3f8729d45
ludie lambright <ludiefletcher@aol.com>
2022-02-22 20:19:16 -0800

    trying to fix linting

--- client/src/components/LoginForm.js
+++ client/src/components/LoginForm.js
@@ -1,3 +1,4 @@
+/* eslint-disable */
 // see SignupForm.js for comments
 import React, { useState } from "react";
 import { Form, Button, Alert } from "react-bootstrap";
--- client/src/components/SignupForm.js
+++ client/src/components/SignupForm.js
@@ -1,3 +1,4 @@
+/* eslint-disable */
 import React, { useState } from "react";
 import { Form, Button, Alert } from "react-bootstrap";
 import { useMutation } from "@apollo/client";
